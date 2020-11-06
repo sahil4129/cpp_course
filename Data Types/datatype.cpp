171272207c21d0e36f4f21cc0324e58c1f25462b
sahil4129 <sahil@simyog.com>
2020-11-06 15:57:52 +0530

    add data type modules

---
+++ Data Types/datatype.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int marks = 100;
+    char temp = 'a';
+    bool is_true = false;
+    bool is_false = true;
+    float marks_p = 85.9;
+    double marks_a = 78.9999999;
+    string str = "Hello World";
+
+    cout<<marks<<endl; 
+    cout<<temp<<endl;
+    cout<<is_true<<endl;
+    cout<<is_false<<endl;
+    cout<<marks_p<<endl;
+    cout<<marks_a<<endl;
+    cout<<str<<endl;
+    
+    return 0;
+}
