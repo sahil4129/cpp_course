4dbee988744667495b0d5a8ca2ee9d49168457cf
sahil4129 <sahil@simyog.com>
2021-02-10 14:40:40 +0530

    add

--- Introduction/helloworld.cpp
+++ Introduction/helloworld.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main(){
+    #if (__linux__) || (_MSC_VER >=1920)
+    cout <<"hh";
+    #endif
     cout<<"Hello World"<<endl;
     return 0;
 }
---
+++ string and pointers/string.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //part1
+    string str = "My name";
+    string str2 = "is sahil";
+    //cout<<str<<endl;
+    //cout<<str + " " +str2<<endl;
+    //cout<< str.length()<<endl;
+    //cout<<str[3]<<endl;
+
+    //part2
+
+    string str_in ="";
+    string str2_in ="";
+   // cin>>str_in;
+    // cout<<"your Input "<<endl;
+    // cout<<str_in<<endl;
+    cout<<"2nd ----"<<endl;
+    
+    getline (cin, str2_in);
+    cout<<"your Input "<<endl;
+    cout<<str2_in<<endl;
+    return 0;
+}
